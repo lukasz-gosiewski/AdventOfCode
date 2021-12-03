460995af163cec06f17123bec8de8af0c7b88d77
Lukasz Gosiewski <lukaszgosiewski.work@gmail.com>
2021-12-03 09:32:14 +0100

    Day 1 done

--- src/main/scala/Day1/Main.scala
+++ src/main/scala/Day1/Main.scala
@@ -8,7 +8,33 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("Day 1")
 
-    val bufferedSource = Source.fromResource("input-1-1.txt")
+    val resultA = solutionA("input-1.txt")
+    val resultB = solutionB("input-1.txt")
+    println(s"Variant A solution: $resultA")
+    println(s"Variant B solution: $resultB")
+  }
+
+  def solutionB(fileName: String): Int = {
+    val bufferedSource = Source.fromResource(fileName)
+    val slidingIterator = bufferedSource.getLines().sliding(3, 1)
+    var previousNumber = slidingIterator.next().map(_.toInt).toList.sum
+    var counter = 0
+
+    for (lines <- slidingIterator) {
+      val number = lines.map(_.toInt).toList.sum
+
+      if (number > previousNumber)
+         counter += 1
+
+      previousNumber = number
+    }
+    bufferedSource.close
+
+    counter
+  }
+
+  def solutionA(fileName: String): Int = {
+    val bufferedSource = Source.fromResource(fileName)
     var counter = 0
     var previousNumber = bufferedSource.getLines().next().toInt
 
@@ -19,10 +45,9 @@ object Main {
 
       previousNumber = line.toInt
     }
-
-    println(s"The answer is: $counter")
-
     bufferedSource.close
+
+    counter
   }
 
 }
