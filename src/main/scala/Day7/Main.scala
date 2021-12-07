c6d81b469444cb9e83c3619078df584a0d373b73
Lukasz Gosiewski <lukaszgosiewski.work@gmail.com>
2021-12-07 06:40:48 +0100

    Day 7 done

---
+++ src/main/scala/Day7/Main.scala
@@ -0,0 +1,30 @@
+package com.gosiewski
+package Day7
+
+import scala.io.Source
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    println("Day 7")
+
+    val crabsPositions = Source.fromResource("input-7.txt")
+      .getLines()
+      .next()
+      .split(",")
+      .map(_.toInt)
+
+    val result = (0 to crabsPositions.max).map(position => {
+      crabsPositions.map(crabsPosition => (position - crabsPosition).abs)
+        .sum
+    }).min
+
+    val result2 = (0 to crabsPositions.max).map(position => {
+      crabsPositions.map(crabsPosition => (0 to (crabsPosition - position).abs).sum)
+        .sum
+    }).min
+
+    println(result)
+    println(result2)
+  }
+}
