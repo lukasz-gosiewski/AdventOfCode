a1ec35e3aa118fb7587a49f73a43e5f10e80c409
Lukasz Gosiewski <lukaszgosiewski.work@gmail.com>
2021-12-10 08:46:14 +0100

    Day 10 done

--- src/main/scala/Day10/Day10.scala
+++ src/main/scala/Day10/Day10.scala
@@ -1,17 +1,10 @@
 package com.gosiewski
 package Day10
 
-import scala.collection.mutable
+import scala.annotation.tailrec
 import scala.io.Source
 
 object Day10 extends App {
-
-  private val checkersScoringMap: Map[Char, Int] = Map(
-    ')' -> 3,
-    ']' -> 57,
-    '}' -> 1197,
-    '>' -> 25137,
-  )
 
   private val openingBracketsMap: Map[Char, Char] = Map(
     '(' -> ')',
@@ -20,24 +13,34 @@ object Day10 extends App {
     '<' -> '>'
   )
 
-  private val autocompleteScoringMap: Map[Char, Int] = Map(
-    '(' -> 1,
-    '[' -> 2,
-    '{' -> 3,
-    '<' -> 4,
-  )
+  {
+    val checkersScoringMap: Map[Char, Int] = Map(
+      ')' -> 3,
+      ']' -> 57,
+      '}' -> 1197,
+      '>' -> 25137,
+    )
 
-  private val exercise1Score = getInput.filter(line => !isCorrect(line))
-    .map(line => mapIncorrectLineToScore(line, checkersScoringMap))
-    .sum
+    val autocompleteScoringMap: Map[Char, Int] = Map(
+      ')' -> 1,
+      ']' -> 2,
+      '}' -> 3,
+      '>' -> 4,
+    )
 
-  private val exercise2PartialScores = getInput.filter(line => isCorrect(line))
-    .map(line => mapIncompleteLineToScore(line, autocompleteScoringMap))
-    .sorted
-  private val exercise2FinalScore = exercise2PartialScores(exercise2PartialScores.length / 2)
+    val exercise1Score = getInput.filter(line => !isCorrect(line))
+      .map(line => mapIncorrectLineToScore(line, checkersScoringMap))
+      .sum
 
-  println(s"Exercise 1 score: $exercise1Score")
-  println(s"Exercise 2 score: $exercise2FinalScore")
+    val exercise2PartialScores = getInput.filter(line => isCorrect(line))
+      .map(line => mapIncompleteLineToScore(line, autocompleteScoringMap))
+      .sorted
+
+    val exercise2FinalScore = exercise2PartialScores(exercise2PartialScores.length / 2)
+
+    println(s"Exercise 1 score: $exercise1Score")
+    println(s"Exercise 2 score: $exercise2FinalScore")
+  }
 
   private def getInput: Seq[Seq[Char]] =
     Source.fromResource("input-10.txt")
@@ -46,44 +49,43 @@ object Day10 extends App {
       .map(line => line.toCharArray.toSeq)
 
   private def isCorrect(line: Seq[Char]): Boolean = {
-    val stack: mutable.Stack[Char] = mutable.Stack()
 
-    line.forall(character => {
-      if (openingBracketsMap.contains(character)) {
-        stack.push(character)
-
-        true
-      }
-      else if (character == openingBracketsMap(stack.pop())) true
+    @tailrec
+    def checkRecursive(sequence: Seq[Char], openBrackets: Seq[Char]): Boolean = {
+      if (sequence.isEmpty) true
+      else if (openingBracketsMap.contains(sequence.head)) checkRecursive(sequence.tail, openBrackets :+ sequence.head)
+      else if (sequence.head == openingBracketsMap(openBrackets.last)) checkRecursive(sequence.tail, openBrackets.dropRight(1))
       else false
-    })
+    }
+
+    checkRecursive(line, Seq.empty)
   }
 
   private def mapIncorrectLineToScore(line: Seq[Char], scoringMap: Map[Char, Int]): BigInt = {
-    val stack: mutable.Stack[Char] = mutable.Stack()
 
-    line.foreach(character => {
-      if (openingBracketsMap.contains(character)) stack.push(character)
-      else if (character != openingBracketsMap(stack.pop())) return scoringMap(character)
-    })
+    @tailrec
+    def mapRecursive(sequence: Seq[Char], openBrackets: Seq[Char], scoringMap: Map[Char, Int]): BigInt = {
+      if (sequence.isEmpty) 0
+      else if (openingBracketsMap.contains(sequence.head)) mapRecursive(sequence.tail, openBrackets :+ sequence.head, scoringMap)
+      else if (sequence.head == openingBracketsMap(openBrackets.last)) mapRecursive(sequence.tail, openBrackets.dropRight(1), scoringMap)
+      else scoringMap(sequence.head)
+    }
 
-    BigInt(0L)
+    mapRecursive(line, Seq.empty, scoringMap)
   }
 
-  private def mapIncompleteLineToScore(line: Seq[Char], matchingScoringMap: Map[Char, Int]): BigInt = {
-    val stack: mutable.Stack[Char] = mutable.Stack()
+  private def mapIncompleteLineToScore(line: Seq[Char], scoringMap: Map[Char, Int]): BigInt = {
 
-    line.foreach(character => {
-      if (openingBracketsMap.contains(character)) stack.push(character)
-      else {
-        val stackElement = stack.pop()
+    @tailrec
+    def mapIncompleteLineToItsCompletion(sequence: Seq[Char], lackingBrackets: Seq[Char]): Seq[Char] = {
+      if (sequence.isEmpty) lackingBrackets
+      else if (openingBracketsMap.contains(sequence.head)) mapIncompleteLineToItsCompletion(sequence.tail, lackingBrackets :+ openingBracketsMap(sequence.head))
+      else if (sequence.head == lackingBrackets.last) mapIncompleteLineToItsCompletion(sequence.tail, lackingBrackets.dropRight(1))
+      else throw new IllegalArgumentException("Line is incorrect")
+    }
 
-        if (character != openingBracketsMap(stackElement)) stack.push(stackElement)
-      }
-    })
-
-    stack.toSeq
-      .map(character => matchingScoringMap(character))
+    mapIncompleteLineToItsCompletion(line, Seq.empty)
+      .map(character => scoringMap(character))
       .foldLeft(BigInt(0L))((score, partialScore) => score * 5L + partialScore)
   }
 }
