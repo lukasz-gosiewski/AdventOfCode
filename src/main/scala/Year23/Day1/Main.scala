e9fc932346d1ff7774188fc0eca2908ab6311186
Lukasz Gosiewski <lukaszgosiewski.work@gmail.com>
2023-12-03 19:35:26 +0100

    Day1 part 1 done

--- src/main/scala/Year23/Day1/Main.scala
+++ src/main/scala/Year23/Day1/Main.scala
@@ -1,8 +1,24 @@
 package com.gosiewski
 package Year23.Day1
 
-class Main {
+import java.lang.Character.isDigit
+import scala.io.Source
+
+object Main {
     def main(args: Array[String]): Unit = {
-        
+        println(solutionA("2023/input-1.txt"))
+    }
+
+    def solutionA(filename: String): Int = {
+        val lines = Source.fromResource(filename).getLines()
+
+        lines.map(line => mapLineToNumber(line))
+            .sum
+    }
+
+    def mapLineToNumber(line: String): Int = {
+        val numbers = line.toCharArray.filter(s => isDigit(s))
+
+        s"${numbers.head}${numbers.last}".toInt
     }
 }
